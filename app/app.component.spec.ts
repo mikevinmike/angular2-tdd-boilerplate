db8db5f90a564854891d6fb268e322bb83c61f1c
Michael Dabernig <michael.dabernig@fh-kufstein.ac.at>
2016-03-30 17:07:53 +0200

    working on unit test on DOM

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import {it, describe, expect, beforeEach, beforeEachProviders, inject} from 'angular2/testing';
-import {TestComponentBuilder, ComponentFixture, injectAsync} from 'angular2/testing';
-import {MockDirectiveResolver} from 'angular2/testing';
+import {TestComponentBuilder, ComponentFixture, injectAsync, MockXHR} from 'angular2/testing';
+import {MockDirectiveResolver, MockViewResolver} from 'angular2/testing';
 import {provide, DirectiveResolver, ViewResolver} from 'angular2/core';
 import {AppComponent} from './app.component';
 import {Hero} from './app.component';
@@ -8,8 +8,7 @@ import {Hero} from './app.component';
 describe("AppComponent", () => {
 
    beforeEachProviders(() => [
-      AppComponent,
-      TestComponentBuilder
+      AppComponent
    ]);
 
    it("should exist", inject([AppComponent], (appComponent:AppComponent) => {
@@ -42,26 +41,17 @@ describe("AppComponent", () => {
 
    });
 
-   //it('should update text', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
-   //   return tcb.createAsync(AppComponent)
-   //       .then((fixture: ComponentFixture) => {
-   //          fixture.detectChanges();
-   //          let compiled = fixture.elementRef.nativeElement;
-   //          expect(compiled.querySelector('span')).toHaveText('hello');
-   //          fixture.componentInstance.typing({target:{value:'new val'}});
-   //          fixture.detectChanges();
-   //          expect(compiled.querySelector('span')).toHaveText('new val');
-   //       });
-   //}));
-
-   //it('should render', injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-   //   return tcb.createAsync(AppComponent).then((componentFixture: ComponentFixture) => {
-   //      const element = componentFixture.nativeElement;
-   //      let appComponentInstance = componentFixture.componentInstance;
-   //      componentFixture.detectChanges();
-   //      expect(element.querySelectorAll('h1').length).toBe(1);
-   //   });
-   //}));
+   // not working...
+   // solution? http://chariotsolutions.com/blog/post/testing-angular-2-components-unit-tests-testcomponentbuilder/
+   it('should update text', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+      return tcb.createAsync(AppComponent)
+          .then((fixture: ComponentFixture) => {
+             //fixture.detectChanges();
+             //let element = fixture.elementRef.nativeElement;
+             //fixture.detectChanges();
+             //expect(element.querySelectorAll('h1').length).toBe(1);
+          });
+   }));
 
 });
 
