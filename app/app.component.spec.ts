2aea3bc5452471508b7a568fcbb871c78a69212a
Michael Dabernig <michael.dabernig@fh-kufstein.ac.at>
2016-03-30 16:36:09 +0200

    working on DOM test

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,12 +1,15 @@
-import {it, describe, expect, beforeEachProviders, inject} from 'angular2/testing';
+import {it, describe, expect, beforeEach, beforeEachProviders, inject} from 'angular2/testing';
 import {TestComponentBuilder, ComponentFixture, injectAsync} from 'angular2/testing';
+import {MockDirectiveResolver} from 'angular2/testing';
+import {provide, DirectiveResolver, ViewResolver} from 'angular2/core';
 import {AppComponent} from './app.component';
 import {Hero} from './app.component';
 
 describe("AppComponent", () => {
 
    beforeEachProviders(() => [
-      AppComponent
+      AppComponent,
+      TestComponentBuilder
    ]);
 
    it("should exist", inject([AppComponent], (appComponent:AppComponent) => {
@@ -39,14 +42,26 @@ describe("AppComponent", () => {
 
    });
 
-   it('should render', injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-      return tcb.createAsync(AppComponent).then((componentFixture: ComponentFixture) => {
-         const element = componentFixture.nativeElement;
-         let appComponentInstance = componentFixture.componentInstance;
-         componentFixture.detectChanges();
-         expect(element.querySelectorAll('h1').length).toBe(1);
-      });
-   }));
+   //it('should update text', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+   //   return tcb.createAsync(AppComponent)
+   //       .then((fixture: ComponentFixture) => {
+   //          fixture.detectChanges();
+   //          let compiled = fixture.elementRef.nativeElement;
+   //          expect(compiled.querySelector('span')).toHaveText('hello');
+   //          fixture.componentInstance.typing({target:{value:'new val'}});
+   //          fixture.detectChanges();
+   //          expect(compiled.querySelector('span')).toHaveText('new val');
+   //       });
+   //}));
+
+   //it('should render', injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+   //   return tcb.createAsync(AppComponent).then((componentFixture: ComponentFixture) => {
+   //      const element = componentFixture.nativeElement;
+   //      let appComponentInstance = componentFixture.componentInstance;
+   //      componentFixture.detectChanges();
+   //      expect(element.querySelectorAll('h1').length).toBe(1);
+   //   });
+   //}));
 
 });
 
