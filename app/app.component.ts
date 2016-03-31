1aca321a28ad1a7402151130ab3b47be41604ea2
mikevinmike <michael.dabernig@gmail.com>
2016-03-31 15:16:42 +0200

    added code of Tutorial 2 Tour of Heroes (angular.io)

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,20 +7,28 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: `
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-      `
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
     public title = "tour of Heroes";
-    public hero:Hero = {
-        id: 1,
-        name: "Windstorm"
+    public heroes = HEROES;
+    public selectedHero:Hero;
+
+    public onSelect(hero:Hero) {
+        this.selectedHero = hero;
     }
 }
+
+let HEROES:Hero[] = [
+    {"id": 11, "name": "Mr. Nice"},
+    {"id": 12, "name": "Narco"},
+    {"id": 13, "name": "Bombasto"},
+    {"id": 14, "name": "Celeritas"},
+    {"id": 15, "name": "Magneta"},
+    {"id": 16, "name": "RubberMan"},
+    {"id": 17, "name": "Dynama"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
