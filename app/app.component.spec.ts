57d37313f3a6af46cd62ac3da6a6e4783e18a94e
mikevinmike <michael.dabernig@gmail.com>
2016-03-31 15:26:47 +0200

    unified quote style

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -23,15 +23,15 @@ describe('AppComponent', () => {
         expect(appComponent).toBeDefined();
     }));
 
-    it("should have title", inject([AppComponent], (appComponent:AppComponent) => {
+    it('should have title', inject([AppComponent], (appComponent:AppComponent) => {
         expect(appComponent.title).toBeDefined();
     }));
 
-    it("should have hero", inject([AppComponent], (appComponent:AppComponent) => {
+    it('should have hero', inject([AppComponent], (appComponent:AppComponent) => {
         expect(appComponent.hero).toBeDefined();
     }));
 
-    describe("hero", function () {
+    describe('hero', function () {
 
         let hero:Hero;
 
@@ -39,11 +39,11 @@ describe('AppComponent', () => {
             hero = appComponent.hero;
         }));
 
-        it("should have id", () => {
+        it('should have id', () => {
             expect(hero.id).toBeDefined();
         });
 
-        it("should have name", () => {
+        it('should have name', () => {
             expect(hero.name).toBeDefined();
         });
 
@@ -65,11 +65,11 @@ describe('AppComponent', () => {
                 fixture.detectChanges();
                 let element = fixture.nativeElement;
                 let appComponent = fixture.componentInstance;
-                appComponent.hero.name = 'Sepp';
+                appComponent.hero.name = 'George';
                 fixture.detectChanges();
                 let subheaders = element.querySelectorAll('h2');
                 expect(subheaders.length).toBe(1);
-                expect(subheaders.item(0).innerText.includes('Sepp')).toBe(true);
+                expect(subheaders.item(0).innerText.includes('George')).toBe(true);
             });
     }));
 
@@ -81,27 +81,27 @@ describe('AppComponent', () => {
                 let appComponent = fixture.componentInstance;
                 let input = element.querySelectorAll('input').item(0);
                 // look if the binding into the DOM works
-                appComponent.hero.name = 'Sepp';
+                appComponent.hero.name = 'George';
                 fixture.detectChanges();
-                expect(input.value).toEqual('Sepp');
+                expect(input.value).toEqual('George');
                 // look if the binding back from the DOM into the Compontent works
-                input.value = 'Hugo';
+                input.value = 'Lara';
                 input.dispatchEvent(new Event('input'));
                 fixture.detectChanges();
                 tick();
-                expect(appComponent.hero.name).toEqual('Hugo');
+                expect(appComponent.hero.name).toEqual('Lara');
             });
     })));
 
 });
 
-describe("Hero", () => {
+describe('Hero', () => {
 
     beforeEachProviders(() => [
         Hero
     ]);
 
-    it("should exist", inject([Hero], (hero:Hero) => {
+    it('should exist', inject([Hero], (hero:Hero) => {
         expect(hero).toBeDefined();
     }));
 
