ce48e42c68acd9528787617799ef1a547013eb58
mikevinmike <michael.dabernig@gmail.com>
2016-03-31 10:08:50 +0200

    introduced a DOM test

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,14 +1,34 @@
-import {it, describe, expect, beforeEach, beforeEachProviders, inject} from 'angular2/testing';
-import {AppComponent} from './app.component';
+import {
+    it,
+    describe,
+    expect,
+    beforeEachProviders,
+    inject,
+    TestComponentBuilder,
+    ComponentFixture,
+    injectAsync
+} from "angular2/testing";
+import {AppComponent} from "./app.component";
 
 describe("AppComponent", () => {
 
-   beforeEachProviders(() => [
-      AppComponent
-   ]);
+    beforeEachProviders(() => [
+        AppComponent
+    ]);
 
-   it("should exist", inject([AppComponent], (appComponent:AppComponent) => {
-      expect(appComponent).toBeDefined();
-   }));
+    it("should exist", inject([AppComponent], (appComponent:AppComponent) => {
+        expect(appComponent).toBeDefined();
+    }));
 
+    it('should render a header', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+        return tcb.createAsync(AppComponent)
+            .then((fixture:ComponentFixture) => {
+                fixture.detectChanges();
+                let element = fixture.elementRef.nativeElement;
+                let appComponent = fixture.componentInstance;
+                fixture.detectChanges();
+                expect(element.querySelectorAll('h1').length).toBe(1);
+            });
+    }));
+    
 });
