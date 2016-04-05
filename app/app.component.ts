c4097ad4ad0b5db8500527fae924c4e27e8cad60
Michael Dabernig <michael.dabernig@fh-kufstein.ac.at>
2016-04-05 13:59:30 +0200

    added code for tutorial 4 - Services (on angular.io)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,32 @@
 import {Component} from "angular2/core";
+import {OnInit} from 'angular2/core';
 import {Hero} from "./hero";
 import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     public title = 'Tour of Heroes';
-    public heroes = HEROES;
+    public heroes: Hero[];
     public selectedHero:Hero;
 
-    public onSelect(hero:Hero) {
+    constructor(private _heroService: HeroService) { }
+
+    ngOnInit():void {
+        this.getHeroes();
+    }
+
+    public onSelect(hero:Hero):void {
         this.selectedHero = hero;
     }
-}
 
-let HEROES:Hero[] = [
-    {"id": 11, "name": "Mr. Nice"},
-    {"id": 12, "name": "Narco"},
-    {"id": 13, "name": "Bombasto"},
-    {"id": 14, "name": "Celeritas"},
-    {"id": 15, "name": "Magneta"},
-    {"id": 16, "name": "RubberMan"},
-    {"id": 17, "name": "Dynama"},
-    {"id": 18, "name": "Dr IQ"},
-    {"id": 19, "name": "Magma"},
-    {"id": 20, "name": "Tornado"}
-];
+    private getHeroes():void {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+}
